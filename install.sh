89d69b3f32762631c0f54188f27fceb909a904da
mbm_1607 <muhammadkhan1607@gmail.com>
2022-07-31 17:03:18 +0500

    Make install.sh enable yarn

--- install.sh
+++ install.sh
@@ -46,6 +46,8 @@ then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
   source ~/.bashrc
   nvm install 16
+  source ~/.bashrc
+  corepack enable yarn
 fi
 
 # Install rbenv and latest ruby
