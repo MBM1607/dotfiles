814e921d9997353990e451c328f7a2699cce7987
mbm_1607 <muhammadkhan1607@gmail.com>
2022-07-31 17:05:09 +0500

    Install gnome-tweaks

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ done
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
 # Install apt packages
-sudo apt install direnv httpie bat python-is-python3 tilix gcc make libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev
+sudo apt install direnv httpie bat python-is-python3 tilix gcc make libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev gnome-tweaks
 
 # Install nvm and node 16
 if ! command -v nvm &> /dev/null
