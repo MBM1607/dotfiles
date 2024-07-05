0d657c266e2055a2385a002afd7e3c0d35e6cbdf
mbm_1607 <muhammadkhan1607@gmail.com>
2024-07-05 15:03:10 +0500

    :bug: Fix proton vpn install

--- scripts/install-proton-vpn.sh
+++ scripts/install-proton-vpn.sh
@@ -6,8 +6,7 @@ NC='\e[0m'
 if ! command -v protonvpn-cli &>/dev/null; then
   echo -e "\n${GREEN}Installing Proton VPN CLI...${NC}"
   wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb -O proton_repo.deb &&
-    sudo apt install ./proton_repo.deb &&
+    sudo dpkg -i proton_repo.deb &&
     rm ./proton_repo.deb &&
-    sudo apt update &&
-    sudo apt install protonvpn-cli
+    sudo apt update
 fi
