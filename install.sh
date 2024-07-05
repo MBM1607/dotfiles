d9d623a2086b779f5e22e2cbb3acdae0ad5ba3c6
mbm_1607 <muhammadkhan1607@gmail.com>
2024-07-05 15:13:55 +0500

    :zap: Create seperate installer for vscode

--- install.sh
+++ install.sh
@@ -66,17 +66,7 @@ if ! command -v deno &>/dev/null; then
 fi
 
 # install vs code
-if ! command -v code &>/dev/null; then
-  echo -e "\n${GREEN}Installing VS Code...${NC}"
-  sudo apt-get install wget gpg &&
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg &&
-    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg &&
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' &&
-    rm -f packages.microsoft.gpg &&
-    sudo apt install apt-transport-https &&
-    sudo apt update &&
-    sudo apt install code
-fi
+./scripts/install-vscode.sh
 
 # install remmina
 if ! command -v remmina &>/dev/null; then
