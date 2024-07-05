f47f0f90d6c7d696f539c1083961daee1c93912c
mbm_1607 <muhammadkhan1607@gmail.com>
2024-07-05 14:45:10 +0500

    :bug: Fix install fonts

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -8,14 +8,14 @@ folder="$HOME/.local/share/fonts"
 # Install iosevka fonts
 # Term Slab for Terminal Use
 # Slab for Normal Use
-repo="be5invis/iosevka"
+repo="be5invis/Iosevka"
 tag="$(latest_git_release $repo)"
 version="${tag:1}"
 url_prefix="https://github.com/$repo/releases/download"
-name_prefix="super-ttc-sgr-iosevka"
+name_prefix="SuperTTC-SGr-Iosevka"
 if [ -n "$version" ]; then
-  wget -q --show-progress "$url_prefix/$tag/$name_prefix-fixed-slab-$version.zip" -O slab.zip &&
-    wget -q --show-progress "$url_prefix/$tag/$name_prefix-term-slab-$version.zip" -O term.zip &&
+  wget -q --show-progress "$url_prefix/$tag/${name_prefix}FixedSlab-$version.zip" -O slab.zip &&
+    wget -q --show-progress "$url_prefix/$tag/${name_prefix}TermSlab-$version.zip" -O term.zip &&
     unzip slab.zip -d "$folder" "*.ttc" &&
     unzip term.zip -d "$folder" "*.ttc" &&
     rm slab.zip term.zip
