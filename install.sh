13fe3e500056cf451cf8198cf3964400bbe9d715
mbm_1607 <muhammadkhan1607@gmail.com>
2024-07-05 14:54:27 +0500

    :zap: Move proton vpn install out of main file

--- install.sh
+++ install.sh
@@ -167,14 +167,7 @@ if ! command -v slack &>/dev/null; then
 fi
 
 # install proton vpn cli
-if ! command -v protonvpn-cli &>/dev/null; then
-  echo -e "\n${GREEN}Installing Proton VPN CLI...${NC}"
-  wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb -O proton_repo.deb &&
-    sudo apt install ./proton_repo.deb &&
-    rm ./proton_repo.deb &&
-    sudo apt update &&
-    sudo apt install protonvpn-cli
-fi
+./scripts/install-proton-vpn.sh
 
 # install foliate
 if ! command -v com.github.johnfactotum.Foliate &>/dev/null; then
---
+++ scripts/install-proton-vpn.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+GREEN='\e[32m'
+NC='\e[0m'
+
+if ! command -v protonvpn-cli &>/dev/null; then
+  echo -e "\n${GREEN}Installing Proton VPN CLI...${NC}"
+  wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb -O proton_repo.deb &&
+    sudo apt install ./proton_repo.deb &&
+    rm ./proton_repo.deb &&
+    sudo apt update &&
+    sudo apt install protonvpn-cli
+fi
